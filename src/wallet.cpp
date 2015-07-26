8f20dfba10099e644ef29e05215dd1c3c789628f
Extrememist <jacobms109@gmail.com>
2015-07-26 19:27:33 +1200

    Testing

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -22,7 +22,7 @@ extern int nStakeMaxAge;
 // mapWallet
 //
 
-std::vector<unsigned char> CWallet::GenerateNewKey(bool bCompressed = true)
+
 struct CompareValueOnly
 {
     bool operator()(const pair<int64, pair<const CWalletTx*, unsigned int> >& t1,
@@ -32,7 +32,7 @@ struct CompareValueOnly
     }
 };
 
-CPubKey CWallet::GenerateNewKey()
+std::vector<unsigned char> CWallet::GenerateNewKey(bool bCompressed = true)
 {
     bool fCompressed = bCompressed ? CanSupportFeature(FEATURE_COMPRPUBKEY) : false; // default to compressed public keys if we want 0.6.0 wallets
 
